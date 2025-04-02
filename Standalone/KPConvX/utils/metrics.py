52b6c8fecf3afdf6949e2d81981b751cafeeb166
lgleznah <luisglez02@gmail.com>
2025-04-02 09:32:34 +0200

    Metrics bugfix for 1-element predictions

--- Standalone/KPConvX/utils/metrics.py
+++ Standalone/KPConvX/utils/metrics.py
@@ -53,8 +53,8 @@ def fast_confusion(true, pred, label_values=None):
     """
 
     # Ensure data is in the right format
-    true = np.squeeze(true)
-    pred = np.squeeze(pred)
+    true = np.squeeze(true) if len(true) != 1 else true
+    pred = np.squeeze(pred) if len(pred) != 1 else pred
     if len(true.shape) != 1:
         raise ValueError('Truth values are stored in a {:d}D array instead of 1D array'. format(len(true.shape)))
     if len(pred.shape) != 1:
